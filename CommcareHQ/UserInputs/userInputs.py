6e405dbb81ed6a90a34cc47ff47a29999168f7fd
Kiran T <67914792+kiran-tripathy@users.noreply.github.com>
2020-08-10 11:30:32 +0530

    Update userInputs.py

--- CommcareHQ/UserInputs/userInputs.py
+++ CommcareHQ/UserInputs/userInputs.py
@@ -26,9 +26,9 @@ class UserInputs:
 
     # inputs for login page
     driver_path = Path(str(Path.home())+"\AutomationProjects\SeleniumCCHQ\Drivers\chromedriver.exe")
-    url = "https://staging.commcarehq.org/accounts/login/"
-    login_username = "automation.user.commcarehq@gmail.com"
-    login_password = "pass@123"
+    url = ""
+    login_username = ""
+    login_password = ""
 
     # inputs for mobile worker page
     mobile_worker_username = "user89"
